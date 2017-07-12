76c71342ace0d8a143f8545a7a7352748d46485b
Gant2134 <Sonic25100@yahoo.com>
2017-07-12 12:12:00 -0700

    Initial commit
    base code wrtten on July 12 2017,
    includes base utilities and underconstruction config_param class.

---
+++ THIS_CLASS_THO/THIS_CLASS_THO/Configuration_Parameters.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace THIS_CLASS_THO
+{
+    public class Configuration_Parameters
+    {
+        //by making the default constructor private it becomes impossible for a third party user to create an 
+        //object of type configuration_parameters Without providing a file name. 
+        private Configuration_Parameters()
+        {
+
+        }
+        
+        public Configuration_Parameters(String configFileName)
+        {
+            //
+
+        }
+
+        String user;
+        String password;
+        String host;
+        String current_database;
+    }
+}
---
+++ THIS_CLASS_THO/THIS_CLASS_THO/Utilities.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace THIS_CLASS_THO
+{
+    class Utilities
+    {
+        public static String Stringify(String input)
+        {
+            return "'" + input + "'";
+        }
+
+        public static void UnitTest()
+        {
+            String input = "Test";
+            input = Stringify(input);
+            Console.WriteLine(input);
+        }
+       
+    }
+}
